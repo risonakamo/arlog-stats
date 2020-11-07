4aeab252c011c0fce9c43256334143412292dde9
risonakamo <hmykanae@gmail.com>
2020-11-07 16:31:58 -0500

    short names combine config

--- @types/arlog-types.d.ts
+++ @types/arlog-types.d.ts
@@ -14,3 +14,12 @@ interface LogRow extends LogRowExtracted
 
 // log rows sorted by their short names
 type LogRowsByShortName=Record<string,LogRow[]>
+
+// map shortnames on the right side to the left side. all names
+// on left side will be considered the right side.
+type ShortNameMerge=Record<string,string[]>
+
+// activating short name merge config to be more usable. left side is now
+// an input shortname, right side is the shortname that in input shortname
+// should be converted to. essentially a reversed version of ShortNameMerge
+type ActivatedShortNameMerge=Record<string,string>
--- arlog-stat.ts
+++ arlog-stat.ts
@@ -1,9 +1,15 @@
 import fs from "fs";
 import _ from "lodash";
+import chalk from "chalk";
 
 import {parseLogEntries} from "./logentry-processors";
 
+// path to log file to use
 const _targetFile:string="testlog.log";
+
+const _combineNames:ShortNameMerge={
+
+};
 
 function main():void
 {
@@ -11,21 +17,42 @@ function main():void
 
     var logEntries:LogRow[]=parseLogEntries(logfile);
 
-    var groupedLogs:LogRowsByShortName=groupByShortName(logEntries);
+    var groupedLogs:LogRowsByShortName=groupByShortName(logEntries,_combineNames);
 }
 
 // TODO: extend this to use shortname merging later.
 // group entries by their shortname.
-function groupByShortName(entries:LogRow[]):LogRowsByShortName
+function groupByShortName(entries:LogRow[],combineNames:ShortNameMerge={}):LogRowsByShortName
 {
+    var activatedcombinednames:ActivatedShortNameMerge=activateShortnameMerge(combineNames);
+
     var res:LogRowsByShortName=_.groupBy(entries,(x:LogRow)=>{
         return x.shortName;
     });
 
-    console.log("unique shortnames:");
+    console.log(chalk.yellow("unique shortnames:"));
     console.log(_.keys(res).sort());
+    console.log();
 
     return res;
+}
+
+// convert shortname merge config into activated short name merge
+function activateShortnameMerge(combineNames:ShortNameMerge):ActivatedShortNameMerge
+{
+    return _.reduce(combineNames,(r:ActivatedShortNameMerge,x:string[],i:string)=>{
+        for (var y=0,l=x.length;y<l;y++)
+        {
+            if (r[x[y]])
+            {
+                console.log(`${chalk.red("shortname merge overlap:")} ${r[x[y]]} -> ${i}`);
+            }
+
+            r[x[y]]=i;
+        }
+
+        return r;
+    },{});
 }
 
 main();
